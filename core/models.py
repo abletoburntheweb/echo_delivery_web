b83ee44502665aa9591d5f9f7d501e0a0f5f56e2
Olejechka <144726472+Olejechka@users.noreply.github.com>
2025-11-06 17:16:53 +0300

    restore files

---
+++ core/models.py
@@ -0,0 +1,73 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Company(models.Model):
+    id_company = models.AutoField(primary_key=True, db_column='id_company')
+    name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    email = models.EmailField()
+    address = models.TextField()
+
+    class Meta:
+        db_table = 'company'
+        verbose_name = 'Компания'
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    id_category = models.AutoField(primary_key=True, db_column='id_category')
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = 'category'
+        verbose_name = 'Категория'
+    def __str__(self):
+        return self.name
+
+
+class Dish(models.Model):
+    id_dish = models.AutoField(primary_key=True)
+    id_category = models.ForeignKey(Category,on_delete=models.CASCADE,db_column='id_category')
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    img = models.CharField(max_length=255, blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        db_table = 'dish'
+        verbose_name = 'Блюдо'
+
+    def __str__(self):
+        return self.name
+
+
+class Ordr(models.Model):
+    id_order = models.AutoField(primary_key=True)
+    id_company = models.ForeignKey(Company,on_delete=models.CASCADE,db_column='fk_id_company')
+    delivery_date = models.DateField()
+    delivery_time = models.TimeField(blank=True, null=True)
+    delivery_address = models.TextField()
+    status = models.CharField(max_length=20, default='новый')
+
+    class Meta:
+        db_table = 'ordr'
+        verbose_name = 'Заказ'
+
+    def __str__(self):
+        return f"Заказ №{self.id_order} от {self.delivery_date}"
+
+
+class OrdrItem(models.Model):
+    id_ordritem = models.AutoField(primary_key=True)
+    id_ordr = models.ForeignKey(Ordr,on_delete=models.CASCADE,db_column='id_ordr')
+    id_dish = models.ForeignKey(Dish,on_delete=models.CASCADE,db_column='id_dish')
+    quantity = models.IntegerField(default=1)
+
+    class Meta:
+        db_table = 'ordritem'
+        verbose_name = 'Позиции заказов'
+
+    def __str__(self):
+        return f"{self.quantity} × {self.id_dish.name} (заказ №{self.id_ordr.id_order})"
---
+++ core/urls.py
@@ -0,0 +1,24 @@
+# core/urls.py
+from django.urls import path
+from django.views.generic import RedirectView
+from . import views
+
+urlpatterns = [
+    path('', RedirectView.as_view(url='login/', permanent=True)),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
+    path('calendar/', views.calendar_view, name='calendar'),
+    path('cart/', views.cart_view, name='cart'),
+    path('menu/', views.menu_view, name='menu'),
+    path('dish/<int:dish_id>/', views.dish_detail_view, name='dish_detail'),
+    path('add_to_cart/', views.add_to_cart, name='add_to_cart'),
+    path('update_cart/', views.update_cart, name='update_cart'),
+    path('receipt/', views.receipt_view, name='receipt'),
+    path('faq/', views.faq_view, name='faq'),
+    path('profile/', views.profile_view, name='profile'),
+    path('agreement/', views.agreement_view, name='agreement'),
+
+    path('admin/menu/', views.admin_menu_view, name='admin_menu'),
+    path('admin/category/add/', views.add_category_view, name='add_category'),
+    path('admin/dish/<int:dish_id>/', views.admin_dish_detail_view, name='admin_dish_detail'),
+]
