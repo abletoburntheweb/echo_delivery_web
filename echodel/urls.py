ed9521bcdd51d3505e392d6dad41859dcbe9ec20
Olejechka <144726472+Olejechka@users.noreply.github.com>
2025-11-06 18:05:56 +0300

    add urls

--- echodel/urls.py
+++ echodel/urls.py
@@ -8,10 +8,15 @@ from core import views
 
 urlpatterns = [
     path('admin/dish/update/<int:dish_id>/', views.update_dish, name='update_dish'),
+    path('admin/panel/', views.admin_panel_view, name='admin_panel'),
+    path('admin/clients/', views.admin_clients_view, name='admin_clients'),
     path('admin/calendar/', views.admin_calendar_view, name='admin_calendar'),
+    path('admin/orders/', views.admin_orders_by_date_view, name='admin_orders_by_date'),
+    path('admin/order/delete/<int:order_id>/', views.admin_delete_order_view, name='admin_delete_order'),
     path('admin/cart/', views.cart_view, name='admin_cart'),
     path('admin/menu/', views.admin_menu_view, name='admin_menu'),
     path('admin/dish/<int:dish_id>/', views.admin_dish_detail_view, name='admin_dish_detail'),
+    path('admin/clients/delete/<int:company_id>/', views.delete_client_view, name='delete_client'),
     path('admin/category/add/', views.add_category_view, name='add_category'),
     path('admin/category/delete/', views.delete_category_view, name='delete_category'),
     path('admin/dish/add/', views.add_dish_view, name='add_dish'),
